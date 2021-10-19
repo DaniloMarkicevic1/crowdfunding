a057aebfbc8ae930868f1ee4913086e238510982
DaniloMarkicevic1 <danilo.markicevic1@gmail.com>
2021-10-19 11:21:46 +0200

    MobileView

--- scripts/app.js
+++ scripts/app.js
@@ -10,13 +10,66 @@ const backProjectBtn = document.querySelector('.backProjectBtn');
 const modal = document.querySelector('.modal');
 const thankYou = document.querySelector('.thankYou');
 const closeModal = document.querySelector('.closeModal');
+// Pledges, RadioButtons
+const pledgesRadioBtns = document.querySelectorAll('input[name="pledge"]');
+const enterPledge = document.querySelectorAll('.enterPledge');
+const modalItem = document.querySelectorAll('.modalItem');
 const body = document.querySelector('body');
 
 body.addEventListener('click', (e) => {
     toggleNav(e.target.classList[0]);
     toggleModal(e.target.classList[0]);
+    pickReward(e.target.classList[0]);
+    highlightPledge();
+    thankYouModal(e.target.innerText);
 });
 
+function pickReward(value) {
+    switch (value) {
+        case 'selectMin':
+            pledgesRadioBtns[0].checked = false;
+            pledgesRadioBtns[1].checked = true;
+            pledgesRadioBtns[2].checked = false;
+            pledgesRadioBtns[3].checked = false;
+            break;
+        case 'selectMed':
+            pledgesRadioBtns[0].checked = false;
+            pledgesRadioBtns[1].checked = false;
+            pledgesRadioBtns[2].checked = true;
+            pledgesRadioBtns[3].checked = false;
+            break;
+        case 'selectHigh':
+            pledgesRadioBtns[0].checked = false;
+            pledgesRadioBtns[1].checked = false;
+            pledgesRadioBtns[2].checked = false;
+            pledgesRadioBtns[3].checked = true;
+            break;
+    }
+}
+function thankYouModal(innerText) {
+    if (innerText === 'Continue') {
+        thankYou.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+        modal.classList.add('hidden');
+        body.classList.add('bodyOverflow');
+    }
+    if (innerText === 'Got it!') {
+        thankYou.classList.add('hidden');
+        overlay.classList.add('hidden');
+        body.classList.remove('bodyOverflow');
+    }
+}
+function highlightPledge() {
+    pledgesRadioBtns.forEach((btn, i) => {
+        if (btn.checked) {
+            enterPledge[i].classList.remove('enterPledge');
+            modalItem[i].classList.add('picked');
+        } else {
+            enterPledge[i].classList.add('enterPledge');
+            modalItem[i].classList.remove('picked');
+        }
+    });
+}
 function toggleNav(value) {
     switch (value) {
         case 'hamburger':
@@ -25,23 +78,28 @@ function toggleNav(value) {
             navHamburger.src = closeIcon;
             navOverlay.classList.add('linearBackground');
             nav.classList.remove('hide');
+            body.classList.add('bodyOverflow');
             break;
         case 'close':
         case 'logo':
         case 'navOverlay':
+        case 'closeNav':
         case 'getStarted':
             navHamburger.classList.add('hamburger');
             navHamburger.classList.remove('close');
             navHamburger.src = hamburgerIcon;
             navOverlay.classList.remove('linearBackground');
             nav.classList.add('hide');
+            body.classList.remove('bodyOverflow');
             break;
     }
 }
 function toggleModal(value) {
     switch (value) {
         case 'backProjectBtn':
-        case 'selectReward':
+        case 'selectMed':
+        case 'selectMin':
+        case 'selectHigh':
         case 'getStarted':
             overlay.classList.remove('hidden');
             modal.classList.remove('hidden');
